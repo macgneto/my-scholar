7e6b216545b25db993ab6c78e77f8daaacfa55ac
droussis <d.roussis.83@gmail.com>
2022-02-23 00:03:42 +0200

    fix Thymeleaf list

--- src/main/java/com/example/myscholar/controllers/StudentController.java
+++ src/main/java/com/example/myscholar/controllers/StudentController.java
@@ -3,6 +3,7 @@ package com.example.myscholar.controllers;
 import com.example.myscholar.repositories.StudentRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,11 +12,12 @@ public class StudentController {
     private final StudentRepository studentRepository;
 
     public StudentController(StudentRepository studentRepository) {
+
         this.studentRepository = studentRepository;
     }
 
     @RequestMapping("/students")
-    public  String getStudents(Model model){
+    public String getStudents(Model model){
 
         model.addAttribute("students", studentRepository.findAll());
         return "students/list";
